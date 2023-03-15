549c58a8ede6eb1810481d1273d18c3a5fa38d9f
Lidiia Kalakutska <lidia.kalakutskaya@gmail.com>
2023-03-15 14:57:01 +0100

    created forms for login and registration

---
+++ code/index.js
@@ -0,0 +1,17 @@
+import {createPage, createLoginForm, createRegistrationForm} from "./functions.js"
+
+
+if ((!localStorage.isLogin) &&
+    !(document.location.pathname.includes('/login.html') ||
+    document.location.pathname.includes('/registration.html')))
+    document.location = '/login.html';
+    
+if (document.location.pathname.includes('/login.html')){
+    createPage('../img/girl.svg');
+    createLoginForm();
+}  
+
+if (document.location.pathname.includes('/registration.html')){
+    createPage('../img/boy.svg');
+    createRegistrationForm();
+}
